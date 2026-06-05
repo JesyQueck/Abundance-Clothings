bd4595e313d6d0089ac00ea5b370533c54b6af22
JesyQueck <oyedokunolumuyiwa07@gmail.com>
2026-06-05 02:57:35 +0100

    font ixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,42 @@
 import type { Metadata } from "next";
-import { Bebas_Neue, DM_Sans, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 import { CartProvider } from "@/context/CartContext";
 import { WishlistProvider } from "@/context/WishlistContext";
 import { AppChrome } from "@/components/layout/AppChrome";
 
-const bebas = Bebas_Neue({
-  weight: "400",
-  subsets: ["latin"],
+const bebas = localFont({
+  src: [
+    {
+      path: "../../Bebas_Neue,DM_Sans,JetBrains_Mono/Bebas_Neue/BebasNeue-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
   variable: "--font-bebas",
   display: "swap",
 });
 
-const dmSans = DM_Sans({
-  weight: ["400", "500", "600", "700"],
-  subsets: ["latin"],
+const dmSans = localFont({
+  src: [
+    {
+      path: "../../Bebas_Neue,DM_Sans,JetBrains_Mono/DM_Sans/DMSans-VariableFont_opsz,wght.ttf",
+      weight: "100 900",
+      style: "normal",
+    },
+  ],
   variable: "--font-dm-sans",
   display: "swap",
 });
 
-const jetbrains = JetBrains_Mono({
-  weight: ["400", "500"],
-  subsets: ["latin"],
+const jetbrains = localFont({
+  src: [
+    {
+      path: "../../Bebas_Neue,DM_Sans,JetBrains_Mono/JetBrains_Mono/JetBrainsMono-VariableFont_wght.ttf",
+      weight: "100 900",
+      style: "normal",
+    },
+  ],
   variable: "--font-jetbrains",
   display: "swap",
 });
@@ -38,10 +53,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${bebas.variable} ${dmSans.variable} ${jetbrains.variable}`}>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      </head>
       <body className="bg-background-deep text-text-primary font-sans antialiased">
         <CartProvider>
           <WishlistProvider>
